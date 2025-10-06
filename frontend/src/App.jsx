48c046bde752548f539d0cfbf7c7b5fc7ac795bc
Your Name <your_email@example.com>
2025-10-06 14:47:34 +0530

    Minor typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 
-import CreatePage from "./pages/CreatePage";
+import CreatePage from "./pages/Createpage";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 
